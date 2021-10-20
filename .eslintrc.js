e241afde7680d702d4bc1ef011cea2ad3347fb52
Andrew Peterson <andy@ndpsoftware.com>
2021-10-19 18:02:31 -0700

    upgrade to @babel/eslint-parser

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,7 +4,7 @@ module.exports = {
     'plugin:promise/recommended',
     'eslint:recommended',
   ],
-  parser:          'babel-eslint',
+  parser:          '@babel/eslint-parser',
   'parserOptions': {
     'ecmaVersion':  6,
     'sourceType':   'module',
